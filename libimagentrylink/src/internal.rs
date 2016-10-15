4b53885f55bbe4c4a7e4eaeeabec7781d1e8b56a
Matthias Beyer <mail@beyermatthias.de>
2016-10-15 14:30:04 +0200

    Impl From<Iterator<Item = Link>> for LinkIter

--- libimagentrylink/src/internal.rs
+++ libimagentrylink/src/internal.rs
@@ -71,6 +71,12 @@ pub mod iter {
 
     }
 
+    impl<I> From<I> for LinkIter where I: Iterator<Item = Link> {
+        fn from(other: I) -> LinkIter {
+            LinkIter(other.into_iter())
+        }
+    }
+
     impl Iterator for LinkIter {
         type Item = Link;
 
